ab393ff712d16edd8f23abab10c6b3d30265e12b
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-09 20:24:59 -0800

    project setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const myFunction = () => {
+  console.log('test');
+};
+
+myFunction();
+myFunction();
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: ['web', 'es5'],
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-arrow-functions']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    fallback: {
+      fs: false
+    }
+  }
+};
