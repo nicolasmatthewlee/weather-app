535878bd625f138e2afdd5a416d71b82f3ed6f19
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-12-10 19:28:28 -0800

    added background and search input elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-const myFunction = () => {
-  console.log('test');
-};
+import './reset.css';
+import './styles.css';
 
-myFunction();
-myFunction();
+import MOUNTAINS_IMAGE from './assets/mountains.jpg';
+
+const key = 'f95e40c94a03a4ec0328c71ffffc2eae';
+const root = document.querySelector(':root');
+const body = document.querySelector('body');
+
+function createElement(type, className = '', content = '') {
+  const element = document.createElement(type);
+  element.setAttribute('class', className);
+  if (type == 'img') {
+    element.src = content;
+  } else {
+    element.textContent = content;
+  }
+  return element;
+}
+
+function setInnerHeight() {
+  root.style.setProperty('--innerHeight', window.innerHeight + 'px');
+}
+function setInnerWidth() {
+  root.style.setProperty('--innerWidth', window.innerWidth + 'px');
+}
+window.addEventListener('resize', () => {
+  setInnerHeight();
+  setInnerWidth();
+});
+setInnerHeight();
+setInnerWidth();
+
+const background = createElement('img', 'background', MOUNTAINS_IMAGE);
+
+const form = createElement('form');
+const searchContainer = createElement('div', 'search-container');
+const searchIcon = createElement('img', 'search-icon');
+const searchInput = createElement('input', 'search-input');
+searchInput.setAttribute('placeholder', 'Search for a city...');
+const submit = createElement('input', 'submit');
+submit.setAttribute('type', 'submit');
+submit.setAttribute('style', 'display:none');
+form.append(searchContainer);
+searchContainer.append(searchIcon, searchInput);
+
+body.append(background, form);
